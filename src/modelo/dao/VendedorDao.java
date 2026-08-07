99fb5d113c9f7d83f4fbd6ddf45bb9d2683304da
oliveiraroberto702 <robertvascol702@gmail.com>
2026-08-07 17:47:54 -0300

    DepartamentoDao e VendedorDao Interfaces

---
+++ src/modelo/dao/VendedorDao.java
@@ -0,0 +1,15 @@
+package modelo.dao;
+
+import java.util.List;
+
+import modelo.entidades.Vendedor;
+
+public interface VendedorDao {
+	void insert(Vendedor obj);
+	void update(Vendedor obj);
+	void deleteById(Integer id);
+	Vendedor findById(Integer id);
+	List<Vendedor> findaAll();
+	
+
+}
