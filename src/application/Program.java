05c95cfa5ea3f13088f37e860296c41b21935e74
oliveiraroberto702 <robertvascol702@gmail.com>
2026-08-07 17:32:28 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import modelo.entidades.Departamento;
+import modelo.entidades.Vendedor;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Departamento obj = new Departamento(1,"Books");
-		System.out.println(obj);
+		
+		Vendedor vendedor = new Vendedor(21, "Bob", "bob@gmail.com", new Date(),3000.0, obj);
+		
+		System.out.println(vendedor);
 		
 			
 			
