f75b4554a035e0dd4ff131c92a90d9c81e229a7e
oliveiraroberto702 <robertvascol702@gmail.com>
2026-08-07 17:11:54 -0300

    Departament entity class

---
+++ src/application/Program.java
@@ -0,0 +1,22 @@
+package application;
+
+import modelo.entidades.Departamento;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Departamento obj = new Departamento(1,"Books");
+		System.out.println(obj);
+		
+			
+			
+			
+		
+		
+		
+		
+
+	}
+
+}
